f50ba1d9db369bbba24e88a6ace78cb6dd4c63a8
Ueli Kunz <kunz@ideadapt.net>
2018-11-13 22:15:18 +0100

    import existing rest server with mongodb backend, and intellij debugging stuff (spring devtools).

--- src/test/kotlin/ch/ideadapt/springreactive/ApplicationTests.kt
+++ src/test/kotlin/ch/ideadapt/springreactive/ApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.bendsoft.multitrackrecorderserver
+package ch.ideadapt.springreactive
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-class MultitrackRecorderServerApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
