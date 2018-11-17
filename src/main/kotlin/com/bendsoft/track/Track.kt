6b3ff5503f786baf9a50273603b95851324480fa
Ueli Kunz <kunz@ideadapt.net>
2018-11-17 23:11:44 +0100

    write some more example tests

--- src/main/kotlin/com/bendsoft/track/Track.kt
+++ src/main/kotlin/com/bendsoft/track/Track.kt
@@ -4,5 +4,5 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-class Track(@Id val id: String?, val name: String = "") {
+class Track(@Id val id: String? = null, val name: String = "") {
 }
