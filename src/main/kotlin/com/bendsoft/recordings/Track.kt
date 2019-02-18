61f31d58603a254ffbd3e7a24091242d6d33645b
ben <ben.dudler@gmail.com>
2019-02-19 00:08:39 +0100

    removed id since it is not a Document but part of a recording -> name must be unique though

--- src/main/kotlin/com/bendsoft/recordings/Track.kt
+++ src/main/kotlin/com/bendsoft/recordings/Track.kt
@@ -1,13 +1,11 @@
 package com.bendsoft.recordings
 
-import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection="tracks")
 @TypeAlias("model.track")
 data class Track (
-        @Id val id: String? = null,
         val name: String = "",
         val trackNumber: Int,
         val channelRecordingFiles: List<ChannelRecordingFile> = emptyList()
